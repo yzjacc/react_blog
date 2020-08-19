d9717e0ab0a9e31bf17440b22d50c4f0b1c5d39a
yzjacc@gmail.com <yzjacc@gmail.com>
2020-08-19 09:54:00 +0800

    fix:暂时修改为80端口

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const fs = require('fs')
 const sslify = require('koa-sslify').default
 const { getData,deleteAllTable } = require('./services/db')
 const { updateBlog, getBlogs, getBlogByPage, } = require('./controllers/articles-handler')
-const _isSSL = true;
+const _isSSL = false;
 
 
 const app = new Koa()
