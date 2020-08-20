12c48a2ade1a80abfc28d3563e376616b72a98ea
yzjacc@gmail.com <yzjacc@gmail.com>
2020-08-20 19:52:24 +0800

    feat：dev-1.1更新

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,38 +4,70 @@ import styles from './index.less'
 
 import { useHistory } from "umi";
 import { withRouter } from 'react-router-dom';
-import { complement } from 'ramda';
+import { complement, none } from 'ramda';
 
 
 class Menu extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.saveRef = ref => {this.refDom = ref};
+        this.state = {
+            i: 0,
+            divStyle: {
+                height: `${window.innerHeight}px`
+            },
+            pointStyle: {
+            }
+        };
+        setInterval(() => {
+            this.setState({
+                i: (this.state.i + 1)
+                    % 10
+            })
+            this.setState({
+                divStyle: {
+                    ...this.state.divStyle,
+                    backgroundImage: `url(https://pg12138.oss-cn-beijing.aliyuncs.com/img/2020/00${this.state.i}.jpg)`
+                }
+            })
+        }, 10000)
+    }
+    pop() {
+        this.setState({
+            divStyle: {
+                ...this.state.divStyle,
+                height: "300px"
+            },
+            pointStyle: {
+                display: 'none'
+            }
+        })
+    }
     render() {
         return (
-            <div className={styles.menu}>
+            <div  className={styles.menu} style={this.state.divStyle}>
                 <div className={styles.contain}>
                     <div className={styles.title}>Yuzijun's Blog</div>
                     <div className={styles.link}>
                         <div className={styles.button} onClick={() => {
                             this.props.history.push('/')
-                        }}><div className = {styles.iconfont}>&#xe61e;</div><p>首页</p></div>
+                        }}><div className={styles.iconfont}>&#xe61e;</div><p>首页</p></div>
                         <div className={styles.button} onClick={() => {
                             this.props.history.push('/archive')
-                        }}><div className = {styles.iconfont}>&#xe762;</div><p>档案</p></div>
+                        }}><div className={styles.iconfont}>&#xe762;</div><p>档案</p></div>
                         <div className={styles.button} onClick={() => {
-                            this.props.history.push('/resume.html')
-                        }}><div className = {styles.iconfont}>&#xe7b5;</div><p>简历</p></div>
+                            window.location.href = 'http://www.yzjacc.cn/resume.html'
+                        }}><div className={styles.iconfont}>&#xe7b5;</div><p>简历</p></div>
                         <div className={styles.button} onClick={() => {
                             this.props.history.push('/about')
-                        }}><div className = {styles.iconfont}>&#xe622;                    </div><p>关于</p></div>
-                        {/* <NavLink to="/"></NavLink>
-                <NavLink to="/archive"></NavLink>
-                <div className={styles.resume} onClick={() => window.location.href = '/resume.html'}></div>
-                <NavLink to="/about"></NavLink> */}
+                        }}><div className={styles.iconfont}>&#xe622;</div><p>关于</p></div>
                     </div></div>
-                {/* <div className={styles.position}>
-                <div className={styles.logo}></div>
-            </div> */}
+                <div className={styles.point} onClick={this.pop.bind(this)} style={this.state.pointStyle}>&#xe668;
+</div>
             </div>
         )
     }
 }
+
 export default withRouter(Menu)
