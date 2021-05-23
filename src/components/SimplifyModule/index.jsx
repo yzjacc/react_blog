e06784e0759a718b8ee939453200d2c0d9500cdf
yzjacc@gmail.com <yzjacc@gmail.com>
2021-05-23 16:08:54 +0800

    feat：新增书籍

--- src/components/SimplifyModule/index.jsx
+++ src/components/SimplifyModule/index.jsx
@@ -12,30 +12,35 @@ export default
         super(props);
     }
     render() {
-        let { time, title, content, url, isShowImg = true } = this.props.data
+        let {
+          time,
+          title,
+          content,
+          imgUrl,
+          isShowImg = true,
+          detailUrl,
+        } = this.props.data;
         return (
-            <div className={styles.main}>
-                <div className={styles.title}>
-                    <div className={styles.name}>
-                        {title}
-                    </div>
-                    <div className={styles.time}>
-                        {time}
-                    </div>
-                </div>
-                <div className={styles.essay}>
-                    {isShowImg ? <div className={styles.essImg}>
-                        <img className={styles.img} src={url} />
-                    </div> : null}
-                    <div className={styles.comment}>
-                        {content}
-                    </div>
-                </div>
-                <div className={styles.footer}>
-                    <div className={styles.logo}></div>
-                    <div className={styles.button}>Try it</div>
-                </div>
+          <div className={styles.main}>
+            <div className={styles.title}>
+              <div className={styles.name}>{title}</div>
+              <div className={styles.time}>{time}</div>
             </div>
+            <div className={styles.essay}>
+              {isShowImg ? (
+                <div className={styles.essImg}>
+                  <img className={styles.img} src={imgUrl} />
+                </div>
+              ) : null}
+              <div className={styles.comment}>{content}</div>
+            </div>
+            <div className={styles.footer}>
+              <div className={styles.logo}></div>
+              <a className={styles.button} href={detailUrl}>
+                Try it
+              </a>
+            </div>
+          </div>
         );
     }
 
