3e3796c63d6d54208fb60f7ab77da4a1eb5f711e
yzjacc@gmail.com <yzjacc@gmail.com>
2021-06-02 20:26:23 +0800

    feat：歌曲组件更新

--- src/components/Aside/index.js
+++ src/components/Aside/index.js
@@ -3,23 +3,27 @@ import { NavLink, history} from 'umi'
 import styles from './index.less'
 import Clock from '../Clock'
 import Tag from '../Tag'
+import Music from '../music'
+
 
 export default function Menu() {
 
     return (
-
+        
         <div className={styles.aside}>
             <div className={styles.module}>
-                <h2 className={styles.title}>&#xe6da; 公告</h2>
-                <Clock></Clock>
-            </div>           
+                <h2 className={styles.title}>&#xe6da; EveryDay Music</h2>
+                {/* <Clock></Clock> */}
+                <Music src ={"https://pg12138.oss-cn-beijing.aliyuncs.com/assets/other/Janet%20Seidel-You%20Belong%20To%20Me.mp3"}></Music>
+            </div>    
+
             {/* <div className={styles.module}> */}
                 {/* <h2>日期 📅</h2> */}
                 {/* <img src = 'https://pg12138.oss-cn-beijing.aliyuncs.com/img/2020/touxiang.jpeg' className={styles.img}></img> */}
                 {/* <Clock></Clock> */}
             {/* </div> */}
             <div className={styles.module}>
-                <h2 className={styles.title}>&#xe62f; 标签</h2>
+                <h2 className={styles.title}>&#xe62f; Tag</h2>
                 <Tag></Tag>
             </div>
             {/* <div className={styles.module}>
