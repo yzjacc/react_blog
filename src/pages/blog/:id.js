cc5554f7fdd22f6aff84e74b734440ca50b12efa
yzjacc <yzjacc@gmail.com>
2022-02-08 11:38:16 +0800

    fix: 🐛 blog页bug修复

--- src/pages/blog/:id.js
+++ src/pages/blog/:id.js
@@ -1,51 +1,48 @@
-import React, { useEffect, PureComponent } from "react";
-import { connect } from "dva";
-import MDRender from "../../components/Markdown";
-import { NavLink } from "umi";
-import styles from "./index.less";
-import md from "./2019-06-20-blog-create.md";
+import React,{ useEffect, PureComponent } from 'react';
+import { connect } from "dva"
+import MDRender from '../../components/Markdown'
+import { NavLink } from 'umi'
+import styles from './index.less';
 
-class Blog extends PureComponent {
-  constructor(props) {
+class Blog extends PureComponent{
+
+  constructor(props){
     super(props);
   }
 
-  componentWillMount() {
-    this.props.onGetContents();
+  componentWillMount(){
+    this.props.onGetContents()
   }
 
-  render() {
-    if (this.props.content == undefined) return null;
-    let content;
-    for (let i = 0; i < this.props.total; i++) {
-      if (this.props.content[i].id == this.props.match.params.id) {
-        content = this.props.content[i].content;
+  render(){
+    if(this.props.content == undefined) return null
+    let content
+    for(let i = 0; i < this.props.total; i++){
+      if(this.props.content[i].id == this.props.match.params.id) {
+        content =  this.props.content[i].content;
         break;
       }
-      if (i == this.props.total - 1) {
-        return <div>{404}</div>;
+      if(i == this.props.total -1 ){
+      return (<div>{404}</div>)
       }
     }
     return (
       <div className={styles.content}>
-        <div>
-          <MDRender content={md} isBase64={false} />
-        </div>
+        <div><MDRender content={content} isBase64={false} /></div>
         {/* <div >{this.props.content[this.state.n].id}</div> */}
-      </div>
-    );
+      </div>)
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   content: state.essay.blogs,
-  total: state.essay.blogCount,
-});
+  total: state.essay.blogCount
+})
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   onGetContents() {
-    dispatch({ type: "essay/getContents" });
-  },
-});
+    dispatch({ type: "essay/getContents"})
+  }
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(Blog);
