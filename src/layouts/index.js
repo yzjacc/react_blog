9d9799e8e10c5e2b9544fc49d641c6bf8ceda860
yzjacc <yzjacc@gmail.com>
2022-02-08 11:15:14 +0800

    chore: test

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import './index.less'
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import "./index.less";
 
 export default function index(props) {
   return (
