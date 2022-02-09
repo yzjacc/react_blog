0836ad85b8314b9539a4ca6f478305358028af27
yzjacc <yzjacc@gmail.com>
2022-02-09 15:26:07 +0800

    fix: 🐛 load页格式更改

--- src/pages/load.tsx
+++ src/pages/load.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Spin } from "antd";
-export default () => (
+import { FC } from "react";
+const App: FC = () => (
   <div style={{ paddingTop: 100, textAlign: "center" }}>
     <Spin size="large" />
   </div>
 );
+
+export default App;
